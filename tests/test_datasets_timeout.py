90c44526bd0cb87dec9fbdf9507eb57a0b983852
quincy-kh-chen <kunhsinc@alumni.cmu.edu>
2021-12-16 17:36:58 -0800

    fix: speed up basedataset init
    - remove unused variable annotation_index

---
+++ tests/test_datasets_timeout.py
@@ -0,0 +1,62 @@
+# TODO: merge this test into tests/test_datasets.py once it migrats to pytest.
+import os
+
+import pytest
+
+from dgp.datasets.frame_dataset import FrameSceneDataset
+from dgp.datasets.synchronized_dataset import SynchronizedSceneDataset
+from tests import TEST_DATA_DIR
+
+
+@pytest.mark.parametrize(
+    "split, datum_names, requested_autolabels, only_annotated_datums, use_diskcache, requested_annotations,skip_missing_data,expected_len",
+    [
+        ('train', ['LIDAR', 'CAMERA_01'], None, False, True, ("bounding_box_2d", "bounding_box_3d"), False, 12),
+    ]
+)
+@pytest.mark.timeout(6.5)  # Repeat 10 times, takes ~0.63 second to instantiate FrameSceneDataset.
+def test_create_frame_scene_dataset(
+    split, datum_names, requested_autolabels, only_annotated_datums, use_diskcache, requested_annotations,
+    skip_missing_data, expected_len
+):
+    """Uses parametrized testing to run multiple cases for FrameSceneDataset."""
+
+    scenes_dataset_json = os.path.join(os.path.join(TEST_DATA_DIR, "dgp"), "test_scene", "scene_dataset_v1.0.json")
+    for _ in range(10):
+        dataset = FrameSceneDataset(
+            scenes_dataset_json,
+            split=split,
+            datum_names=datum_names,
+            requested_autolabels=requested_autolabels,
+            only_annotated_datums=only_annotated_datums,
+            use_diskcache=use_diskcache,
+            skip_missing_data=skip_missing_data,
+            requested_annotations=requested_annotations,
+        )
+        assert len(dataset) == expected_len
+
+
+@pytest.mark.parametrize(
+    "split,datum_names,forward_context,backward_context,generate_depth_from_datum,requested_annotations,expected_len", [
+        ('train', ['LIDAR', 'CAMERA_01'], 1, 1, 'LIDAR', ("bounding_box_2d", "bounding_box_3d"), 2),
+    ]
+)
+@pytest.mark.timeout(6.5)  # Repeat 10 times, takes ~0.63 second to instantiate SynchronizedSceneDataset.
+def test_create_sync_scene_dataset(
+    split, datum_names, forward_context, backward_context, generate_depth_from_datum, requested_annotations,
+    expected_len
+):
+    """Uses parametrized testing to run multiple cases for SynchronizedSceneDataset."""
+
+    scenes_dataset_json = os.path.join(os.path.join(TEST_DATA_DIR, "dgp"), "test_scene", "scene_dataset_v1.0.json")
+    for _ in range(10):
+        dataset = SynchronizedSceneDataset(
+            scenes_dataset_json,
+            split=split,
+            datum_names=datum_names,
+            forward_context=forward_context,
+            backward_context=backward_context,
+            generate_depth_from_datum=generate_depth_from_datum,
+            requested_annotations=requested_annotations
+        )
+        assert len(dataset) == expected_len
