8c630166d190ccfa5c0114a23103c2eac51df02b
yuta-tsuzuki-woven <91296855+yuta-tsuzuki-woven@users.noreply.github.com>
2022-10-06 23:31:28 +0000

    docs: update build documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.4-5-g23479ca',
+    VERSION: 'v1.4-6-g0386a84',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
