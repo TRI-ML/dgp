34673464ec9d699a48bd140a2b338b289ed14545
tk-woven <61102060+tk-woven@users.noreply.github.com>
2022-12-07 03:22:22 +0000

    docs: update build documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.4-8-g2fa0170',
+    VERSION: 'v1.4-9-g1791c88',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
