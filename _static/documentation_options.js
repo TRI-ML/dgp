6124dd3296d7158ae8ce56526df1e9661e9fa3f2
rachel-lien <103277350+rachel-lien@users.noreply.github.com>
2023-04-05 15:46:40 +0000

    docs: update build documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.5-6-gbe004be',
+    VERSION: 'v1.5-7-gc8c1473',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
