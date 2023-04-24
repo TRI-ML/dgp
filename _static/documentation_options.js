0a5c940064b97a46de5c62a11c3cd3c63a7965ae
chunleng-woven <120554638+chunleng-woven@users.noreply.github.com>
2023-04-24 02:42:39 +0000

    docs: update build documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.5-8-gcf305f6',
+    VERSION: 'v1.5-9-g2493d71',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
