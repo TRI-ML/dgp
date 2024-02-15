c43cbb829641c5bbc357b3d409e8be4b33f1886f
chrisochoatri <91976672+chrisochoatri@users.noreply.github.com>
2024-02-15 23:10:26 +0000

    docs: update build documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v1.5-22-gac4c2a6',
+    VERSION: 'v1.5-23-g1860c50',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
