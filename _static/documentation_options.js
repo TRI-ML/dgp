3a3b60285e2882fc26b7d4ed982d2c668eafe5ee
kunlin596 <kun.lin.596@gmail.com>
2026-02-12 10:17:56 +0000

    docs: update build documentation

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v2.0.1-1-g93f32a7',
+    VERSION: 'v2.0.1-2-ge6a76f9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
